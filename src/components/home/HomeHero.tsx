aec52d17f6cd63d109e5e33f3d366f9f85f9b407
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-12-21 15:26:20 +0200

    adds home hero

--- src/components/home/HomeHero.tsx
+++ src/components/home/HomeHero.tsx
@@ -1,3 +1,6 @@
+import Whatsapp from "../icons/home/Whatsapp";
+import WhiteArrow from "../icons/home/WhiteArrow";
+
 const HomeHero = () => {
   return (
     <section
@@ -6,7 +9,52 @@ const HomeHero = () => {
         background: "linear-gradient(0.5deg, #FEFEFE 0.43%, #03316C 99.57%)",
       }}
     >
-      HomeHero
+      <div className="container">
+        <h2 className="lg:w-250 mx-auto text-[#FEFEFE] md:text-5xl text-xl font-semibold leading-[150%] text-center">
+          Manage, Grow, and Optimize Your Sports Business Effortlessly
+        </h2>
+
+        <p className="text-[#EAEAEA] md:text-2xl text-sm font-medium leading-[150%] md:mt-6 mt-3 text-center">
+          “Manage bookings, memberships, and classes—all in one platform built to grow your sports <br /> business.”
+        </p>
+
+        <div className="flex justify-center mt-8">
+          <div className="relative">
+            <button className="w-79.75 h-14 bg-[#FEFEFE33] rounded-4xl text-[#FEFEFE] text-lg font-semibold">
+              Request a Demo
+            </button>
+
+            <div className="absolute top-1/2 -right-6 -translate-y-1/2">
+              <WhiteArrow />
+            </div>
+          </div>
+        </div>
+
+        <div className="mt-14 flex lg:flex-row flex-col items-center justify-center gap-6">
+          <img 
+            src="/images/home/hero_vid.jpg"
+            alt="hero img"
+            className="rounded-4xl opacity-70 md:h-109 h-47.5 md:w-full w-85.75 object-cover"
+          />
+          <div className="relative md:mt-0 -mt-20 md:ml-0 ml-36">
+            <div className="md:w-[384px] w-51 md:h-109 h-54.25 bg-[#FEFEFE] rounded-4xl md:py-12 py-4 md:px-4 px-4">
+            <h2 className="text-[#001B3F] md:text-2xl text-sm font-medium leading-[100%]">Increase Your Revenue</h2>
+            <p className="text-[#565555] md:text-sm text-[10px] font-medium leading-[150%] md:mt-3 mt-2">
+              Boost bookings, memberships, and class attendance with our all-in-one platform.
+            </p>
+            <img 
+              src="/images/home/hero_dashboard.png"
+              alt="dashboard"
+              className="md:mt-11.25 mt-3"
+            />
+          </div>
+
+          <div className="absolute -bottom-10 right-0">
+            <Whatsapp />
+          </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
---
+++ src/components/icons/home/WhiteArrow.tsx
@@ -0,0 +1,11 @@
+const WhiteArrow = () => {
+    return (
+        <svg width="56" height="56" viewBox="0 0 56 56" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect width="56" height="56" rx="28" fill="#FEFEFE"/>
+            <path d="M26.1597 21.6609L34.6383 23.0038L33.2954 31.4824" stroke="#0F0F0F" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M20.8848 32.9962L34.5005 23.1037" stroke="#0F0F0F" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
+    )
+}
+
+export default WhiteArrow
