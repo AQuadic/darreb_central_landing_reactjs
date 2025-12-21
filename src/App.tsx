d7798548662c0b48f79c66a12a0b2d6d6a657de9
moon <ahmedabozaid.dev@gmail.com>
2025-12-21 10:15:55 +0200

    fixes layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import { BrowserRouter, Route, Routes } from "react-router";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./components/general/Layout";
 
 function App() {
   return (
     <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-          </Route>
-
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<div>Home Page</div>} />
+          <Route path="about" element={<div>About Page</div>} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 }
