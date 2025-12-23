447d5774a4147009baaf659e0898e42665fbd07e
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-12-23 15:57:48 +0200

    translate have questions section

--- src/components/faq/HaveQuestions.tsx
+++ src/components/faq/HaveQuestions.tsx
@@ -1,15 +1,18 @@
+import { useTranslation } from "react-i18next";
+
 const HaveQuestions = () => {
+    const { t } = useTranslation("faq");
     return (
         <section className="bg-[#F3F7FC] md:py-12.5 py-4 relative">
             <div className="container flex flex-wrap items-center justify-between">
                 <div>
-                    <h3 className="text-[#001B3F] md:text-[32px] text-base font-semibold leading-[100%] md:text-start text-center">Still have questions?</h3>
+                    <h3 className="text-[#001B3F] md:text-[32px] text-base font-semibold leading-[100%] md:text-start text-center">{t("haveQuestions.heading")}</h3>
                     <p className="md:w-133.5 text-[#565555] md:text-2xl text-sm font-medium leading-[150%] md:mt-6 mt-3">
-                        Can’t find the answers you’re looking for in our FAQs? Reach out to us and we’ll be happy to help.
+                        {t("haveQuestions.description")}
                     </p>
 
                     <button className="w-full h-14 text-[#FEFEFE] text-lg font-semibold mt-8 bg-[linear-gradient(90deg,#6594D0_0%,#071C36_100%)] rounded-4xl">
-                        Contact Us
+                        {t("haveQuestions.button")}
                     </button>
                 </div>
 
